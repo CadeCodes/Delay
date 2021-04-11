c1923d0c16fa9bc42acfe68da61a17958a033367
Mythological <82343551+MythologicalMC@users.noreply.github.com>
2021-04-11 15:40:40 -0700

    Fixed ping check to now check real api
    Ready for use!!!

--- delay.py
+++ delay.py
@@ -1,10 +1,15 @@
 #imports
+import os
 import subprocess
 from colorama import init,Fore, Back, Style
+import asyncio
+import urllib.parse
+from time import perf_counter
 init(convert=True)
 #vars
 ver = "Literal First Release, prob has millions of bugs"
 ping = 0
+pings = []
 tempping = 0
 searches = 0
 base = 400
@@ -18,21 +23,13 @@ def setupPing():
          You can also type "auto" for ping detection! ''')
             #See if they want auto ping detection or manual
     if (tempping == "auto"):
-        #ping the server and read output
-        print (Fore.RESET)
-        p = subprocess.Popen("ping api.mojang.com", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        print()
-        print (Fore.BLUE + " [info]" + Fore.CYAN + " Your avg ping was: " + formatOut(out) + "ms! This value will be used as your ping.")
-        return formatOut(out)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+        return sum(pings)/5
     else:
         print()
         print (Fore.BLUE + " [info]" + Fore.CYAN + " You entered " + str(tempping) + " as your ping in ms, This value will be used as your ping.")
         return tempping
-def formatOut(stf):
-    format1 = str(stf).split("Average = ",1)[1]
-    formatted = format1.split("m",1)[0]
-    return formatted
 def setupSearches():
     print()
     print (Fore.BLUE, end='')
@@ -54,7 +51,27 @@ def calc():
 def out():
     print()
     print (Fore.BLUE + " [DELAY]" + Fore.CYAN + " Your approximate delay to use is: " + Fore.WHITE + str(delay) + Fore.CYAN + " ms. This is an estimate, and you should lower or higher it depending on request times." )
+#THIS PING CHECK CODE IS FROM kingscratss#3407 on discord!!
+async def check(url: str):
+    async def x():
+        uri = urllib.parse.urlparse(url)
+        reader, writer = await asyncio.open_connection(uri.hostname, 443, ssl=True)
+        writer.write(f"GET {uri.path or '/'} HTTP/1.1\r\nHost:{uri.hostname}\r\n\r\n".encode())
+
+        start = perf_counter()
+        await writer.drain()
+
+        await reader.read(1)
+        end = perf_counter()
+        return round((end - start) * 1000)
+    for _ in range(5):
+        pings.append(await x())
+        await asyncio.sleep(0.01)
+async def main():
+    await check("https://api.minecraftservices.com/minecraft")
+
 #cool ascii text thing/creds
+os.system("cls")
 print(Fore.LIGHTBLUE_EX + '''
        _      _
       | |    | |
